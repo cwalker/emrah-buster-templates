a27e955912c24c08a84c0d249ecc33624913d74a
cwalker <arafikov@gmail.com>
2022-07-30 18:47:37 +0400

    Update mod_token_affiliation.lua

--- machines/eb-jitsi/usr/share/jitsi-meet/prosody-plugins/mod_token_affiliation.lua
+++ machines/eb-jitsi/usr/share/jitsi-meet/prosody-plugins/mod_token_affiliation.lua
@@ -27,58 +27,7 @@ module:hook("muc-occupant-joined", function (event)
     if context_user then
         if context_user["affiliation"] == "owner" then
             affiliation = "owner"
-        elseif context_user["id"] ~= nil then
-            affiliation = "owner"
-        elseif context_user["affiliation"] == "teacher" then
-            affiliation = "owner"
-        end
-    end
-
-    module:log(LOGLEVEL, "affiliation: %s", affiliation)
-    room:set_affiliation(true, occupant.bare_jid, affiliation)
-end)
-
-
-
-local LOGLEVEL = "debug"
-
-local is_admin = require "core.usermanager".is_admin
-local is_healthcheck_room = module:require "util".is_healthcheck_room
-module:log(LOGLEVEL, "loaded")
-
-local function _is_admin(jid)
-    return is_admin(jid, module.host)
-end
-
-module:hook("muc-occupant-joined", function (event)
-    local room, occupant = event.room, event.occupant
-
-    if is_healthcheck_room(room.jid) or _is_admin(occupant.jid) then
-        module:log(LOGLEVEL, "skip affiliation, %s", occupant.jid)
-        return
-    end
-
-    if not event.origin.auth_token then
-        module:log(LOGLEVEL, "skip affiliation, no token")
-        return
-    end
-
-    local affiliation = "member"
-    local origin = event.origin    
-    local context_user = event.origin.jitsi_meet_context_user
-    
-    if origin then
-        if origin ["aud"] == "RocketChat" then
-            affiliation = "owner"
-        end
-    end
-        
-    if context_user then
-        if context_user["affiliation"] == "owner" then
-            affiliation = "owner"
-        elseif context_user["affiliation"] == "moderator" then
-            affiliation = "owner"
-        elseif context_user["affiliation"] == "teacher" then
+        elseif context_user["id"] then
             affiliation = "owner"
         end
     end
