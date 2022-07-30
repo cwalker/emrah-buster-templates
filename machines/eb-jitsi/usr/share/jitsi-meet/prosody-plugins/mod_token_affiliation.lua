4f9444c51d31a7cb0576393c5c93675f29c5ce54
cwalker <arafikov@gmail.com>
2022-07-30 17:42:36 +0400

    Update mod_token_affiliation.lua

--- machines/eb-jitsi/usr/share/jitsi-meet/prosody-plugins/mod_token_affiliation.lua
+++ machines/eb-jitsi/usr/share/jitsi-meet/prosody-plugins/mod_token_affiliation.lua
@@ -27,7 +27,7 @@ module:hook("muc-occupant-joined", function (event)
     if context_user then
         if context_user["affiliation"] == "owner" then
             affiliation = "owner"
-        elseif context_user["id"] > "1" then
+        elseif context_user["id"] ~= nil then
             affiliation = "owner"
         elseif context_user["affiliation"] == "teacher" then
             affiliation = "owner"
